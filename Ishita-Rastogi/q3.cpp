714434cb38ff5012a6b7416168829781ef59a17d
Ishita466 <77580341+Ishita466@users.noreply.github.com>
2021-05-14 20:59:07 +0530

    Create q3.cpp

---
+++ Ishita-Rastogi/q3.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{   Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+    int T;
+    cin>>T;
+
+    while(T--)
+    {
+        int K;
+        cin>>K;
+        Node *head= NULL;
+        Node *temp = head;
+
+        for(int i=0;i<K;i++){
+        int data;
+        cin>>data;
+            if(head==NULL)
+            head=temp=new Node(data);
+            else
+            {
+                temp->next = new Node(data);
+                temp=temp->next;
+            }
+        }
+
+        Node *result = removeDuplicates(head);
+        print(result);
+        cout<<endl;
+    }
+    return 0;
+}
+Node *removeDuplicates(Node *root)
+{
+ Node* head = root;
+ if(!head) return NULL;
+ while(head->next!=NULL){
+     if(head->data == head->next->data){
+     head->next = head->next->next;
+     }
+     else head=head->next;
+ }
+     return root;
+     // your code goes here
+};
